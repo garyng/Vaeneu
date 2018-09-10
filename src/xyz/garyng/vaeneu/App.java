0913af3fa5f5198b4d4d2aa740817bfc9c1d9187
GaryNg <garyng.zhongbo@gmail.com>
2018-09-11 02:25:07 +0800

    feat: add error view

--- src/xyz/garyng/vaeneu/App.java
+++ src/xyz/garyng/vaeneu/App.java
@@ -5,11 +5,19 @@ import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewTuple;
 import de.saxsys.mvvmfx.guice.MvvmfxGuiceApplication;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import xyz.garyng.vaeneu.Error.ErrorView;
 import xyz.garyng.vaeneu.Main.LoginView;
 import xyz.garyng.vaeneu.Main.LoginViewModel;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class App extends MvvmfxGuiceApplication
 {
@@ -21,11 +29,31 @@ public class App extends MvvmfxGuiceApplication
     @Override
     public void startMvvmfx(Stage primaryStage) throws Exception
     {
+        Thread.currentThread().setUncaughtExceptionHandler((thread, throwable) ->
+        {
+            try
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Error/ErrorView.fxml"));
+                Parent root = loader.load();
+                ErrorView controller = loader.getController();
+                String message = Arrays.stream(throwable.getStackTrace()).map(Object::toString).collect(Collectors.joining("\n"));
+                controller.taMessage.setText(message);
+
+                Stage stage = new Stage();
+                stage.setTitle("Something went wrong...");
+                stage.setScene(new Scene(root, 600, 500));
+                stage.showAndWait();
+                Platform.exit();
+            } catch (IOException e)
+            {
+            }
+        });
+
+
         primaryStage.setTitle("Vaeneu - University Venue Management");
         ViewTuple<LoginView, LoginViewModel> main = FluentViewLoader.fxmlView(LoginView.class).load();
 
         JFXDecorator decorator = new JFXDecorator(primaryStage, main.getView());
-
 
         Scene scene = new Scene(decorator, 600, 600);
         ObservableList<String> stylesheets = scene.getStylesheets();
---
+++ src/xyz/garyng/vaeneu/Error/ErrorView.java
@@ -0,0 +1,9 @@
+package xyz.garyng.vaeneu.Error;
+
+import javafx.scene.control.TextArea;
+
+public class ErrorView
+{
+    public TextArea taMessage;
+
+}
