3b45462979ef5b2be9124851e6dc8a28f9b273af
GaryNg <garyng.zhongbo@gmail.com>
2018-09-15 20:06:56 +0800

    feat(query): add loggers to query dispatcher

--- src/xyz/garyng/vaeneu/Query/IQueryDispatcher.java
+++ src/xyz/garyng/vaeneu/Query/IQueryDispatcher.java
@@ -6,5 +6,9 @@ import java.util.Optional;
 public interface IQueryDispatcher
 {
     <TQuery extends IQuery, TResult> Optional<TResult> Dispatch(Class<TResult> resultType, TQuery parameter);
+
+    /**
+     * Dispatch a query that returns a list of {@link TData}.
+     */
     <TQuery extends IQuery, TData> Optional<List<TData>> DispatchList(Class<TData> innerDataType, TQuery parameter);
 }
--- src/xyz/garyng/vaeneu/Query/QueryDispatcher.java
+++ src/xyz/garyng/vaeneu/Query/QueryDispatcher.java
@@ -5,6 +5,7 @@ import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 import com.google.inject.util.Types;
+import lombok.extern.slf4j.Slf4j;
 import net.jodah.typetools.TypeResolver;
 
 import javax.management.openmbean.TabularData;
@@ -13,6 +14,7 @@ import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class QueryDispatcher implements IQueryDispatcher
 {
     private Injector _injector;
@@ -29,6 +31,7 @@ public class QueryDispatcher implements IQueryDispatcher
     {
         ParameterizedType handlerType = Types.newParameterizedType(IQueryHandler.class, parameter.getClass(), resultType);
         TypeLiteral<IQueryHandler<TQuery, TResult>> l = (TypeLiteral<IQueryHandler<TQuery, TResult>>) TypeLiteral.get(handlerType);
+        _logger.debug("[{}] {}", l, parameter);
         return _injector.getInstance(Key.get(l)).Execute(parameter);
     }
 
@@ -39,6 +42,7 @@ public class QueryDispatcher implements IQueryDispatcher
         ParameterizedType listType = Types.newParameterizedType(List.class, innerDataType);
         ParameterizedType handlerType = Types.newParameterizedType(IQueryHandler.class, parameter.getClass(), listType);
         TypeLiteral<IQueryHandler<TQuery, List<TData>>> l = (TypeLiteral<IQueryHandler<TQuery, List<TData>>>) TypeLiteral.get(handlerType);
+        _logger.debug("[{}] {}", l, parameter);
         return _injector.getInstance(Key.get(l)).Execute(parameter);
     }
 }
